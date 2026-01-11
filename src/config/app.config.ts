21b79ff66cca15fac607cfe4b4ffa2d09191b241
marcodev1x <contatomarcodev@gmail.com>
2026-01-11 14:05:55 -0300

    chore: adjusted service to generic. Started to setup a new payment service.

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,25 +1,13 @@
 const {
     DEFAULT_PORT,
     LOCAL,
-    AI_API_KEY,
-    AI_API_URL,
-    AI_API_MODEL,
-    BCRYPT_HASH_QUANTITY,
-    RENDER_BACKEND_URL,
     REDIS_URL,
-    AI_DEFAULT_TRACKING_URL,
     PRINCIPAL_FRONT_URL,
 } = process.env;
 
 export const appConfig = {
     local: LOCAL,
     port: DEFAULT_PORT,
-    aiApiKey: AI_API_KEY,
-    aiApiUrl: AI_API_URL,
-    aiApiModel: AI_API_MODEL,
-    bcryptHashQuantity: Number(BCRYPT_HASH_QUANTITY),
-    renderBackendUrl: RENDER_BACKEND_URL,
-    aiDefaultTrackingUrl: AI_DEFAULT_TRACKING_URL,
     redisUrl: REDIS_URL,
     principalFrontUrl: PRINCIPAL_FRONT_URL,
 };
